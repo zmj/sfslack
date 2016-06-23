fe2e04c5c5915c44f5b094a2966fb2d20f00d5ca
zmj <zach.jeyakaran@gmail.com>
2016-06-23 09:42:43 -0400

    new authid

--- sfauth.go
+++ sfauth.go
@@ -10,7 +10,7 @@ func TestLogin() SfLogin {
 	account := SfAccount{"jeffcombscom", "sharefile.com", "sf-api.com"}
 	authCookie := http.Cookie{
 		Name:  "SFAPI_AuthID",
-		Value: "421e60ff-7721-4002-a492-3060a3c594a4"}
+		Value: "a7622b87-3fff-4caf-97dd-dd7ddb78057d"}
 
 	cookieUrl, _ := url.Parse(account.BaseUrl())
 	jar, _ := cookiejar.New(nil)
