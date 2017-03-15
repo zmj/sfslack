a42ce2294cca2f6f0c6ac27e0b896fc79e01b999
zmj <zach.jeyakaran@gmail.com>
2017-03-15 15:48:07 -0400

    workflow handler

--- server/server.go
+++ server/server.go
@@ -42,7 +42,7 @@ func NewServer(cfg config) (*http.Server, error) {
 	}
 	srv := &Server{
 		authCache: sharefile.NewAuthCache(cfg.OAuthID, cfg.OAuthSecret),
-		wfCache:   &wfCache{
+		workflows:   &wfCache{
 			mu: &sync.Mutex{},
 			runners: make(map[int]*runner),
 		},
@@ -57,10 +57,26 @@ func (srv *server) handler() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", printReq)
 	mux.HandleFunc(commandPath, srv.newCommand)
-	mux.HandleFunc(commandClickPath, srv.newCommandClick)
-	mux.HandleFunc(authPath, srv.authCallback)
-	mux.HandleFunc(eventPath, srv.eventCallback)
+	mux.HandleFunc(commandClickPath, srv.wfHandler(srv.newCommandClick))
+	mux.HandleFunc(authPath, srv.wfHandler(srv.authCallback))
+	mux.HandleFunc(eventPath, srv.wfHandler(srv.eventCallback))
 	return mux
+}
+
+func (srv *server) wfHandler(h func(*runner, http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(wr http.ResponseWriter, req *http.Request) {
+		wfID, err := wfID(req)
+		if err != nil {
+			http.Error(wr, err.Error(), http.StatusBadRequest)
+			return
+		}
+		wf, ok := srv.workflows.get(wfID)
+		if !ok {
+			http.Error(wr, "Workflow not found", http.StatusInternalServerError)
+			return
+		}
+		h(wf, wr, req)
+	}
 }
 
 func (c *wfCache) new(cmd slack.Command) *runner {
@@ -70,6 +86,13 @@ func (c *wfCache) new(cmd slack.Command) *runner {
 	r := newRunner(cmd, c.wfID)
 	c.runners[c.wfID] = r
 	return r
+}
+
+func (c *wfCache) get(int wfID) (*runner, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	r, ok := c.runners[wfID]
+	return r, ok
 }
 
 func wfID(req *http.Request) (int, error) {
