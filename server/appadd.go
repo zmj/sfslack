e32988825ecd4eded6a3e698e16ba1db6934a850
zmj <zach.jeyakaran@gmail.com>
2017-03-19 15:00:27 -0400

    file

---
+++ server/appadd.go
@@ -0,0 +1,12 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
+func (srv *server) AppAdded(wr http.ResponseWriter, req *http.Request) {
+	bytes, _ := httputil.DumpRequest(req, true)
+	fmt.Println(string(bytes))
+}
